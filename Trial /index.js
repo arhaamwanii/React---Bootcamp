a52ed6da9e8bd9fcfafbd22fa9a82f1c22acb1c2
arhaamwanii <arhamwani765@gmail.com>
2023-12-02 01:38:09 +0530

    .

--- Trial /index.js
+++ Trial /index.js
@@ -1,3 +1,17 @@
+
+const neww = "from this point on new code snippet will be exicuted "
+
+function newSnippet(){
+    console.log("")
+    console.log("")
+    console.log(neww)
+    console.log("")
+    console.log("")
+}
+
+
+//ONE
+
 const loop = {
     one : ["two" , "threee"],
     two : ["four"  , "five" ]
@@ -9,4 +23,12 @@ for(const item of Object.keys(loop)){
     item2 = loop[item]
     console.log(item2);
 }
-    // const item = loop[key]
+
+newSnippet();
+
+//TWO
+
+
+
+
+
