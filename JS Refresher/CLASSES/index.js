a6644953ce2c0afe039b6b92ff27ffd8308b227a
arhaamwanii <arhamwani765@gmail.com>
2023-12-02 07:02:58 +0530

    .

---
+++ JS Refresher/CLASSES/index.js
@@ -0,0 +1,23 @@
+class Employee{
+    constructor(name , age , height, gender){
+        this.name  = name;
+        this.age  = age;
+        this.height = height;
+
+            if (gender) {
+                this.gender =  "male"
+            }
+            else {
+                this.gender = "female"
+            }
+
+    }
+}
+
+const emplyeeOne = new Employee("burhan", 17 , 5.11 , true)
+
+console.log(Employee)
+console.log(emplyeeOne)
+
+
+//CLASSES in JS are used to create a Object using a constructor function peter peter pedd on the peed dog thr  
