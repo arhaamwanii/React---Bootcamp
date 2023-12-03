4fcfdc01b2b8ffe3baa60647fb4486d025e87470
arhaamwanii <arhamwani765@gmail.com>
2023-12-04 00:15:26 +0530

    .

--- essentials/src/App.jsx
+++ essentials/src/App.jsx
@@ -1,14 +1,20 @@
+function Header(){
+  return(
+    <header>
+    <img  style={{borderRadius: "10px"}} src="https://images.pexels.com/photos/18010956/pexels-photo-18010956/free-photo-of-woman-in-white-veil-standing-in-doorway.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      Fundamental React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+  )
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img  style={{borderRadius: "10px"}} src="https://images.pexels.com/photos/18010956/pexels-photo-18010956/free-photo-of-woman-in-white-veil-standing-in-doorway.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header/>
       <main>
         <h2>Time to get started!</h2>
       </main>
@@ -20,4 +26,5 @@ export default App;
 
 
 
-//arham amin wani it is actaully quite  ironic o
+//here we difined a function in the same file and then used it in the same file as a component in another function 
+//it
