487e8caf54ddb49160c06efefa99e57d26970524
arhaamwanii <arhamwani765@gmail.com>
2023-12-03 22:56:44 +0530

    .

--- essentials/src/App.jsx
+++ essentials/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img  style={{borderRadius: "10px"}} src="https://images.pexels.com/photos/18010956/pexels-photo-18010956/free-photo-of-woman-in-white-veil-standing-in-doorway.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           Fundamental React concepts you will need for almost any app you are
@@ -17,3 +17,7 @@ function App() {
 }
 
 export default App;
+
+
+
+//arham amin wani it is actaully quite  ironic o
---
+++ essentials/src/Component/Header.jsx
@@ -0,0 +1,14 @@
+export function Header(){
+    return(
+        <div>
+            <img style={{borderRadius: "10px"}} src="https://images.pexels.com/photos/18010956/pexels-photo-18010956/free-photo-of-woman-in-white-veil-standing-in-doorway.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+            <h1>React Essentials</h1>
+            <p>
+            Fundamental React concepts you will need for almost any app you are
+            going to build!
+            </p>
+        </div>
+    )
+}
+
+
