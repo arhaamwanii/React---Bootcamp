6ca479e8156e3238e47c4472c63ffef7d950df00
arhaamwanii <arhamwani765@gmail.com>
2023-12-08 00:36:02 +0530

    .

--- essentials/src/App.jsx
+++ essentials/src/App.jsx
@@ -63,7 +63,7 @@ function App() {
 
  //by using this sytax all the elements inside of the array CORE_CONCEPTS will passed as Key value pairs to the Call Back}
 //we cant use the normal between opening and clsing tag as we have not rubjected  the told react where to use this shit
-
+//args
 
 
 export default App;
