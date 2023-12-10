68680f9b5927ec93993a1d265b818092fd9f6c2a
arhaamwanii <arhamwani765@gmail.com>
2023-12-10 14:33:21 +0530

    .

--- essentials/src/Components/TabButtons.jsx
+++ essentials/src/Components/TabButtons.jsx
@@ -1,19 +1,23 @@
 export default function TabButton({buttonInfo , onClick , onSelect , props , children}){
    
     
-    function anything(params) {
+    function handleClick(params) {
         console.log("Call Back 01")
     }
 
     return(
         <>
             <li>
-                <button onClick={anything} >{buttonInfo} {children}</button>
+                <button onClick={handleClick} >{buttonInfo} {children}</button>
             </li>
         </>
 
     )
 }
+
+//onClick is inbuilt
+
+
 
 
 //onelect is a prop and we set the value to it
