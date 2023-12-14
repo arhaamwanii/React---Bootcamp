e81e181ba861aa973ff20a0e101947cad4677614
arhaamwanii <arhamwani765@gmail.com>
2023-12-14 20:43:07 +0530

    .

--- module2/src/components/CoreConcept.jsx
+++ module2/src/components/CoreConcept.jsx
@@ -11,4 +11,4 @@ export default function CoreConcept({ image, title, description }) {
 //comment attempt 14 dec
 //comment again on 14 dec 
 // and yet again on 14 dec
-//
+//agian and again and again
